dc6155326a2fcefc041b6fccb37218277d57fcee
Piyush Katole <katolepiyush16@gmail.com>
2026-07-28 12:09:54 +0530

    feat: initialize tailwind configuration with custom theme extensions and plugins

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -77,7 +77,7 @@ export default {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        marquee: "marquee 32s linear infinite",
+        marquee: "marquee 15s linear infinite",
       },
     },
   },
